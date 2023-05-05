bfad20502ba6a1e19036136d1fc87ecb29fb73c7
Thomas Hodges <thodges@javascriptonline.org>
2023-05-05 15:26:30 -0700

    tests

---
+++ tests/integration/components/jumbo-test.js
@@ -0,0 +1,18 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'super-rentals/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | jumbo', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders the content inside a jumbo header with a tomster', async function (assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<Jumbo>Hello world!</Jumbo>`);
+    assert.dom('.jumbo').exists();
+    assert.dom('.jumbo').hasText('Hello world!');
+    assert.dom('.jumbo .tomster').exists();
+  });
+});
